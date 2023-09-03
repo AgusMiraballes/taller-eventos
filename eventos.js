8c4ed1831d2576bf7c65f495d02d813cb025a6b4
AgusMiraballes <agusmiraballesg@gmail.com>
2023-09-03 12:46:43 -0300

    Puntos 6, 7 y 8
    Creo el archivo eventos.js; agrego el div; modifico el estilo del div y agrego un manejador de eventos para el click.

--- eventos.js
+++ eventos.js
@@ -1,5 +1,10 @@
 //Alerta al hacer click
-
 function alerta() {
     alert("¡Hola!");
     }
+
+//Manejador de eventos
+    const div = document.querySelector('div');
+    div.addEventListener('click', () => {
+        alert('¡Hola! Soy el div');
+      });
